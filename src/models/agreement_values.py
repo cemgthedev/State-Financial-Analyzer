bbed86936a907d4a5753f7bddb9960317f0e43ba
Rogi Rocz <rogirocz@gmail.com>
2025-02-25 18:40:06 -0300

    adicionando os atributos da tabela agremment_values

--- src/models/agreement_values.py
+++ src/models/agreement_values.py
@@ -1,7 +1,14 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 # Entidade de valores de convênio
 class AgreementValues(SQLModel, table=True):
     __tablename__ = "agreement_values"  # Table name
     id: int = Field(default=None, primary_key=True)
-    agreement_id: int = Field(foreign_key="agreements.id") # Foreign key to Agreement
+    agreement_id: int = Field(foreign_key="agreements.id", ondelete='CASCADE') # Foreign key to Agreement
+    valor_inicial_total: float
+    valor_inicial_repasse_concedente: float
+    valor_inicial_contrapartida_convenente: float
+    valor_atualizado_total: float
+    valor_pago: float
+    
+    agreement: "Agreement" = Relationship(back_populates="values")  # Relationship with Agreement
