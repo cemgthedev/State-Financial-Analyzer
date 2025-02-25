47fc23053934733ce523603daf602ef706329360
Rogi Rocz <rogirocz@gmail.com>
2025-02-25 13:31:39 -0300

    ajuste para usar configuração local de conexão ao bd

--- src/database.py
+++ src/database.py
@@ -1,8 +1,14 @@
 from sqlmodel import SQLModel, Session, create_engine
 from models import *
+import yaml
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
 # Configuração da URL do banco de dados
-DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/sfa"
+DATABASE_URL = config["database"]["url"]
+if DATABASE_URL is None:
+    DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/sfa"
 
 # Criação do engine
 engine = create_engine(DATABASE_URL)
