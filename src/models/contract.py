d3b2404f505d724bf91f2225bd93dbc9ec8877ee
cemgthedev <cemg.the.dev@gmail.com>
2025-03-02 17:37:36 -0300

    feat: adicionando as tabelas de contratos e valores de contratos

--- src/models/contract.py
+++ src/models/contract.py
@@ -1,6 +1,15 @@
-from sqlmodel import SQLModel, Field
+from typing import List, Optional
+from sqlmodel import Relationship, SQLModel, Field
 
 # Entidade de contrato
 class Contract(SQLModel, table=True):
     __tablename__ = "contracts"  # Table name
     id: int = Field(default=None, primary_key=True)
+    numero_contrato: str = Field(default=None)
+    cpf_cnpj: str = Field(default=None)
+    contratante: str = Field(default=None)
+    contratado: str = Field(default=None)
+    tipo_objeto: Optional[str] = Field(default=None)
+    objeto: str = Field(default=None)
+
+    values: List["ContractValues"] = Relationship(back_populates="contract", sa_relationship_kwargs={"cascade": "all, delete-orphan"})
--- src/models/contract_values.py
+++ src/models/contract_values.py
@@ -1,7 +1,14 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import Relationship, SQLModel, Field
 
 # Entidade de valores de contrato
 class ContractValues(SQLModel, table=True):
     __tablename__ = "contract_values"  # Table name
     id: int = Field(default=None, primary_key=True)
-    contract_id: int = Field(foreign_key="contracts.id") # Foreign key to Contract
+    contract_id: int = Field(foreign_key="contracts.id", ondelete='CASCADE') # Foreign key to Contract
+    valor_original: float
+    valor_aditivo: float
+    valor_atualizado: float
+    valor_empenhado: float
+    valor_pago: float
+    
+    contract: "Contract" = Relationship(back_populates="values")
