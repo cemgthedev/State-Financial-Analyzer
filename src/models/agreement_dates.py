0d31f7d952ca9f865ce9507e9c6fe058dab8c19c
carlosryan2008 <48930497+carlosryan2008@users.noreply.github.com>
2025-03-04 19:54:57 -0300

    Modificando modelo de agreement date

--- src/models/agreement_dates.py
+++ src/models/agreement_dates.py
@@ -1,7 +1,12 @@
 from sqlmodel import SQLModel, Field
+from datetime import date  # Importe o tipo date
 
 # Entidade de datas de convênio
 class AgreementDates(SQLModel, table=True):
     __tablename__ = "agreement_dates"  # Table name
     id: int = Field(default=None, primary_key=True)
-    agreement_id: int = Field(foreign_key="agreements.id") # Foreign key to Agreement
+    agreement_id: int = Field(foreign_key="agreements.id")  # Foreign key to Agreement
+    data_assinatura: date = Field(nullable=True)  # Data de assinatura (pode ser nula)
+    data_termino: date = Field(nullable=True)  # Data de término (pode ser nula)
+    data_publi_ce: date = Field(nullable=True)  # Data de publicação no CE (pode ser nula)
+    data_publi_doe: date = Field(nullable=True)  # Data de publicação no DOE (pode ser nula)
