e864453893b59a9e5853677bbdbf40d08da014e1
cemgthedev <cemg.the.dev@gmail.com>
2025-03-07 03:55:22 -0300

    refactor: correções nas rotas complexas de contratos e função de conversão de data
    uma coluna do último dataset foi normalizada conforme o padrão dos outros datasets, as rotas complexas foram modificadas para gerar os dados corretamente contando também com o maior volume de dados e a rota de conversão de data foi tratada para retornar nulo em casos de erro

--- src/utils/convert_date.py
+++ src/utils/convert_date.py
@@ -7,14 +7,20 @@ Converte uma data no formato dd/mm/aaaa para o formato datetime
 def convert_date(date_value):
     """
     Converte um valor de data para datetime, lidando com diferentes tipos de entrada.
+    Retorna None se a conversão falhar.
     """
-    if pd.isna(date_value):  # Verifica se é NaN e retorna None
+    try:
+        if pd.isna(date_value):  # Verifica se é NaN e retorna None
+            return None
+
+        if isinstance(date_value, datetime):  # Se já for datetime, retorna diretamente
+            return date_value
+
+        if isinstance(date_value, pd.Timestamp):  # Se for Timestamp, converte para string
+            date_value = date_value.strftime("%d/%m/%Y")
+
+        return datetime.strptime(date_value, "%d/%m/%Y")  # Converte string para datetime
+
+    except Exception as e:
+        print(f"Erro ao converter data: {date_value} - {e}")  # Mensagem de erro para depuração
         return None
-
-    if isinstance(date_value, datetime):  # Se já for datetime, retorna diretamente
-        return date_value
-
-    if isinstance(date_value, pd.Timestamp):  # Se for Timestamp, converte para string
-        date_value = date_value.strftime("%d/%m/%Y")  
-
-    return datetime.strptime(date_value, "%d/%m/%Y")  # Converte string para datetime
