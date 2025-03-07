535007f1ff00beaa2acbbd9a2ec8f0ca26969ba8
Rogi Rocz <rogirocz@gmail.com>
2025-03-07 19:09:54 -0300

    Ajustando modelo para aceitar valores nulos e ter valor padrão

--- src/models/agreement_values.py
+++ src/models/agreement_values.py
@@ -5,10 +5,10 @@ class AgreementValues(SQLModel, table=True):
     __tablename__ = "agreement_values"  # Table name
     id: int = Field(default=None, primary_key=True)
     agreement_id: int = Field(foreign_key="agreements.id", ondelete='CASCADE') # Foreign key to Agreement
-    valor_inicial_total: float
-    valor_inicial_repasse_concedente: float
-    valor_inicial_contrapartida_convenente: float
-    valor_atualizado_total: float
-    valor_pago: float
+    valor_inicial_total: float = Field(default=None, nullable=True)
+    valor_inicial_repasse_concedente: float = Field(default=None, nullable=True)
+    valor_inicial_contrapartida_convenente: float = Field(default=None, nullable=True)
+    valor_atualizado_total: float = Field(default=None, nullable=True)
+    valor_pago: float = Field(default=None, nullable=True)
     
     agreement: "Agreement" = Relationship(back_populates="values")  # Relationship with Agreement
